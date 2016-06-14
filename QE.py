5b66a31f2f4be835d3761f8a4e76414f7a09c2b0
ktszk <stm435435@gmail.com>
2016-06-14 18:07:57 +0900

    add make_win

--- QE.py
+++ QE.py
@@ -26,15 +26,14 @@ type_xc='pbe'
 pot_type=['%s.%s-nsp-van','%s.%s-n-van','%s.%s-n-kjpaw_psl.0.1']
 pseude_dir='/home/Apps/upf_files/'
 
-sw_bands=F
-sw_ph=T
-sw_wan=F
-sw_restart=T
+sw_bands = T
+sw_ph = F
+sw_wan = T
+sw_restart = T
 
 sw_run = F
 sw_mpi = T
-mpi_num = 32
-kthreads_num = 4
+(mpi_num,kthreads_num) = (32,4)
 
 #=====================pw_parameters================================
 k_mesh_scf=12
@@ -42,25 +41,32 @@ k_mesh_bands=20
 k_mesh_wannier=8
 ecut=80.0
 ec_rho=800
-conv=1.0e-5
+(e_conv,f_conv)=(1.0e-5,1.0e-4)
+(scf_conv,nscf_conv)=(1.0e-12,1.0e-10)
 nband=50
+nstep=400
 deg=0.025
 eband_win=[-3,3]
 #======================ph_parameters===============================
 q_mesh_dyn=[4,4,4]
 q_mesh_bands=20
 q_mesh_dos=8
+ph_conv=1.0e-14
 pband_win=[0,900]
 #===================Wannier_parameters=============================
 nwann=10                     #number of wannier
 dis_win=[-3.00,4.00]         #max(min)_window
 frz_win=[-0.0,0.0]           #froz_window
-projection=[['Fe','d']]      #projections
+projection=[('Fe','d')]      #projections
 sw_fs_plot= F                #plot Fermi surface
+fermi_mesh = 100
 unk=F                        #Bloch(Wannier)_func
 #======================modules=====================================
 import numpy as np
 import os,datetime
+#=====================global_lambda_expression=====================
+TorF=lambda x:'.True.' if x else '.False.'
+w_conv=lambda a:'1.0E%d'%int(np.log(a))
 #======================pw_&_ph_common_parameter====================
 #k_list=[['G',[0.,0.,0.]],['K',[2./3,0.,0.]],['M',[0.5,-0.5/np.tan(2.*np.pi/3.),0.]],
 #        ['G',[0.,0.,0.]],['Z',[0.,0.,0.5]]] #Phexa
@@ -70,10 +76,10 @@ k_list=[['G',[0.,0.,0.]],['X',[0.0,0.5,-0.5]],['P',[0.25,0.75,-0.25]],
 #================physical parameter================================
 bohr=round(0.52917721092,6)
 ibohr=1.0/bohr
-mass={'H':1.00794,'He':4.002602,
-      'Li':6.941,'Be':9.012182,'B':10.811,'C':12.0107,'N':14.0067,'O':15.9994,'F':18.9984032,'Ne':20.1797,
-      'Na':22.98977,'Mg':24.305,'Al':26.981538,'Si':28.0855,'P':30.973761,'S':32.065,'Cl':35.453,'Ar':39.948,
-      'K':39.0983,'Ca':40.078,'Sc':44.95591,
+mass={'H':1.00794,                                                                            'He':4.002602,
+      'Li':6.941,'Be':9.012182,'B':10.811,'C':12.0107, 'N':14.0067,'O':15.9994,'F':18.9984032,'Ne':20.1797,
+      'Na':22.98977,'Mg':24.305,  'Al':26.981538,'Si':28.0855,'P':30.973761,'S':32.065,'Cl':35.453,'Ar':39.948,
+      'K':39.0983,  'Ca':40.078,  'Sc':44.95591,
       'Ti':47.867,'V':50.9415,'Cr':51.9961,'Mn':54.938049,'Fe':55.845,'Co':58.9332,'Ni':58.6934,'Cu':63.546,
       'Zn':65.409,'Ga':69.723,'Ge':72.64,'As':74.9216,'Se':78.96,'Br':79.904,'Kr':83.798,
       'Rb':85.4678,'Sr':87.62,'Y':88.90585,
@@ -85,14 +91,15 @@ mass={'H':1.00794,'He':4.002602,
       'Hf':178.49,'Ta':180.9479,'W':183.84,'Re':186.207,'Os':190.23,'Ir':192.217,'Pt':195.078,'Au':196.96655,
       'Hg':200.59,'Tl':204.3833,'Pb':207.2,'Bi':208.98038,
       'Th':232.0381,'Pa':231.03588,'U':238.02891}
-#=====================global_variables=============================
+#==========================global_variables========================
 UPF=[pp%(at,type_xc)+'.UPF' for pp,at in zip(pot_type,atom)]
 outdir='./'
 fildvscf="'dvscf'"
 fildyn="'%s.dyn'"%prefix
 flfrc="'%s.fc'"%prefix
-recover='.True.' if sw_restart else '.False.'
+recover=TorF(sw_restart)
 restart="'from_scratch'"
+
 try:
     brav
     try:
@@ -112,13 +119,13 @@ except NameError:
             brav='F'
         elif space in [146,148,155,160,161,166,167]:
             brav='R'
-        elif space in [38,39,40,41]:
+        elif space in range(38,42): #38>41 is A
             brav='A'
         elif space in [5,8,9,12,15,20,21,35,36,37,63,64,65,66,67,68]:
             brav='C'
         else:
             brav='P'
-            if space in [191]:
+            if space in range(168,195): #168>194 is Hexagonal
                 hexa=T
     elif isinstance(space,str):
         if 'I' in space:
@@ -152,10 +159,8 @@ def date():
     from locale import setlocale, LC_ALL
     d=datetime.datetime.today()
     setlocale(LC_ALL,'')
-    if os.environ['LANG'].find('ja')!=-1:
-        print(d.strftime('%Y年  %B %d日 %A %H:%M:%S JST'))
-    else:
-        print(d.strftime('%a %b %d %X JST %Y'))
+    print(d.strftime('%Y年  %B %d日 %A %H:%M:%S JST' if os.environ['LANG'].find('ja')!=-1 
+                     else '%a %b %d %X JST %Y'))
 
 def os_and_print(command):
     print(command)
@@ -189,6 +194,17 @@ def make_fstring_obj(obj_name,var_list,val_dic,sw_form):
         fstring=fstring+form%vc+str(val_dic[vc])+'\n'
     fstring=fstring+'/\n'
     return fstring
+
+def atom_position(atom,atomic_position):
+    mat=get_cr_mat(brav,False)
+    mat=np.linalg.inv(mat).T
+    aposition=[[list(mat.dot(np.array(ap))) for ap in app] for app in atomic_position]
+    atom_string=''
+    for i,at in enumerate(atom):
+        for ap in aposition[i]:
+            atom_string=atom_string+'%2s  %12.9f %12.9f %12.9f\n'%tuple([at]+ap)
+
+    return atom_string
 
 def get_cr_mat(brav,hexa):
     if brav=='P':
@@ -226,26 +242,11 @@ def cell_parameter_stream(axis,deg):
     mat_ax=np.array([[axis[0]*ibohr,0,0],[0,axis[1]*ibohr,0],[0,0,axis[2]*ibohr]])
     mat=get_cr_mat(brav,hexa)
     a_vec=list(mat.dot(mat_ax))
-    cell_string='CELL_PARAMETERS\n'
+    cell_string=''
     for aa in a_vec:
         cell_string=cell_string+'  %12.9f  %12.9f  %12.9f\n'%tuple(aa)
-    cell_string=cell_string+'\n'
 
     return cell_string
-
-def atomic_parameters_stream(atom,atomic_position,UPF):
-    mat=get_cr_mat(brav,False)
-    mat=np.linalg.inv(mat).T
-    aposition=[[list(mat.dot(np.array(ap))) for ap in app] for app in atomic_position]
-    atom_string='\nATOMIC_SPECIES\n'
-    for at,up in zip(atom,UPF):
-        atom_string=atom_string+' %-2s %11.7f  %s\n'%(at,mass[at],up)
-    atom_string=atom_string+'\nATOMIC_POSITIONS crystal\n'
-    for i,at in enumerate(atom):
-        for ap in aposition[i]:
-            atom_string=atom_string+'%2s  %12.9f %12.9f %12.9f\n'%tuple([at]+ap)
-    atom_string=atom_string+'\n'
-    return atom_string
 
 def k_line_stream(k_num,k_list):
     k_string='%d\n'%(k_num*(len(k_list)-1)+1)
@@ -253,23 +254,25 @@ def k_line_stream(k_num,k_list):
     w=1.
     wt=' %f'%w if True else ''
     for kb,ka in zip(k_list,k_list[1:]):
+        ks=kb[1]
         kp=[(kaa-kbb)*dk for kaa,kbb in zip(ka[1],kb[1])]
         for i in range(k_num):
-            k_string=k_string+'%9.6f %9.6f %9.6f'%(kb[1][0]+kp[0]*i,kb[1][1]+kp[1]*i,kb[1][2]+kp[2]*i)+wt+'\n'
+            k_string=k_string+'%9.6f %9.6f %9.6f'%(ks[0]+kp[0]*i,ks[1]+kp[1]*i,ks[2]+kp[2]*i)+wt+'\n'
     k_string=k_string+'%9.6f %9.6f %9.6f'%tuple(k_list[-1][1])+wt+'\n'
     return k_string
 
-def k_cube_stream(k_num,w_sw):
+def k_cube_stream(k_num,w_sw,sw_wan):
+    wfunc=lambda x,y,z:'\n' if x else ' %f\n'%(1./z if y else 1.)
     if not isinstance(w_sw,bool):
         w_sw=False
     if isinstance(k_num,int):
         dk=1./k_num
-        w=1./(k_num**3) if w_sw else 1.
-        k_string='%d\n'%k_point**3
+        wst=wfunc(sw_wan,w_sw,(k_num**3))
+        k_string='%d\n'%k_num**3
         for i in range(k_num):
             for j in range(k_num):
                 for k in range(k_num):
-                    k_string=k_string+'%f %f %f %f\n'%(dk*i,dk*j,dk*k,w)
+                    k_string=k_string+'%f %f %f'%(dk*i,dk*j,dk*k)+wst
     elif isinstance(k_num,list):
         if  len(k_num==2):
             dk=[1./kp for kp in k_num]
@@ -285,12 +288,12 @@ def k_cube_stream(k_num,w_sw):
             w0=1.
             for wi in range(kn):
                 w0=w0*wi
-        w=1./w0 if w_sw else 1.
+        wst=wfunc(sw_wan,w_sw,w0)
         k_string='%d\n'%w0
         for i in range(kn[0]):
             for j in range(kn[1]):
                 for k in range(kn[2]):
-                    k_string=k_string+'%f %f %f %f\n'%(dk[0]*i,dk[1]*j,dk[2]*k,w)
+                    k_string=k_string+'%f %f %f'%(dk[0]*i,dk[1]*j,dk[2]*k)+wst
     else:
         print('Please input list or int into k_point ')
         exit()
@@ -298,14 +301,23 @@ def k_cube_stream(k_num,w_sw):
     return k_string
 
 def make_pw_in(calc):
-    fext='nscf' if calc in ['nscf','bands'] else 'scf'
+    def atomic_parameters_stream(atom,atomic_position,UPF):
+        atom_string='\nATOMIC_SPECIES\n'
+        for at,up in zip(atom,UPF):
+            atom_string=atom_string+' %-2s %11.7f  %s\n'%(at,mass[at],up)
+        atom_string=atom_string+'\nATOMIC_POSITIONS crystal\n'
+        atom_string=atom_string+atom_position(atom,atomic_position)
+        atom_string=atom_string+'\n'
+        return atom_string
+
+    (fext,convthr) =('nscf',nscf_conv) if calc in ['nscf','bands'] else ('scf',scf_conv)
     fname='%s.%s'%(prefix,fext)
     fstream=''
     var_control=['title','calculation','restart_mode','outdir','pseudo_dir',
                  'prefix','etot_conv_thr','forc_conv_thr','nstep','tstress','tprnfor']
     val_control={'title':"'%s'"%prefix,'calculation':"'%s'"%calc,'restart_mode':restart,'outdir':"'%s'"%outdir,
-                 'pseudo_dir':"'%s'"%pseude_dir,'prefix':"'%s'"%prefix,'etot_conv_thr':conv,'forc_conv_thr':conv,
-                 'nstep':400,'tstress':'.True.','tprnfor':'.True.'}
+                 'pseudo_dir':"'%s'"%pseude_dir,'prefix':"'%s'"%prefix,'etot_conv_thr':w_conv(e_conv),
+                 'forc_conv_thr':w_conv(f_conv),'nstep':nstep,'tstress':'.True.','tprnfor':'.True.'}
     fs_control=make_fstring_obj('control',var_control,val_control,'pw')
     fstream=fstream+fs_control
 
@@ -318,7 +330,7 @@ def make_pw_in(calc):
     fstream=fstream+fs_system
 
     var_electrons=['diagonalization','conv_thr']
-    val_electrons={'diagonalization':"'david'",'conv_thr':1.0e-12}
+    val_electrons={'diagonalization':"'david'",'conv_thr':w_conv(convthr)}
     fs_electrons=make_fstring_obj('electrons',var_electrons,val_electrons,'pw')
     fstream=fstream+fs_electrons
 
@@ -337,13 +349,13 @@ def make_pw_in(calc):
     fs_atomparam=atomic_parameters_stream(atom,atomic_position,UPF)
     fstream=fstream+fs_atomparam
     if ibrav==0:
-        fs_cellparam=cell_parameter_stream(axis,deg)
+        fs_cellparam='CELL_PARAMETERS\n'+cell_parameter_stream(axis,deg)+'\n'
         fstream=fstream+fs_cellparam
 
     fstream=fstream+'K_POINTS (%s)\n'%('crystal' if calc in ['nscf, bands'] else 'automatic')
-    if calc in ['nscf, bands']:
-        if sw_cube:
-            fs_kl_param=k_cube_stream(k_mesh_wannier,w_sw)
+    if calc in ['nscf', 'bands']:
+        if calc=='nscf':
+            fs_kl_param=k_cube_stream(k_mesh_wannier,T,F)
         else:
             fs_kl_param=k_line_stream(k_mesh_bands,k_list)
         fstream=fstream+fs_kl_param
@@ -364,21 +376,84 @@ def make_bands_in():
 def make_pw2wan_in():
     fname='%s.pw2wan'%prefix
     fstream='\n'
-    var_bands=[]
-    val_bands={}
-    fs_bands=make_fstring_obj('pw2wan',var_bands,val_bands,'pw2wan')
+    var_bands=['outdir','prefix','seedname','spin_component','write_unk']
+    val_bands={'outdir':"'./'",'prefix':"'%s'"%prefix,'seedname':"'%s'"%prefix,
+               'spin_component':"'none'",'write_unk': TorF(unk)}
+    fs_bands=make_fstring_obj('inputpp',var_bands,val_bands,'pw2wan')
     fstream=fstream+fs_bands
     write_file(fname,fstream)
 
 def make_win():
-    pass
+    def win_strings(values,sname):
+        def format_val(a):
+            if isinstance(a,float):
+                return '%7.4f'%a
+            else:
+                return str(a)
+        if(sname=='plot'):
+            form='%-25s = '
+        else:
+            form='%-12s = '
+        strings=''
+        for vs in values:
+            strings=strings+form%vs[0]+format_val(vs[1])+'\n'
+        strings=strings+'\n'
+        return strings
+    def get_k_point_path():
+        p_name=lambda x: 'GAMMA' if x=='G' else x
+        strings='begin Kpoint_Path\n'
+        for kl1,kl2 in zip(k_list,k_list[1:]):
+            strings=strings+'%-5s'%p_name(kl1[0])+' %5.2f %5.2f %5.2f  '%tuple(kl1[1])
+            strings=strings+'%-5s'%p_name(kl2[0])+' %5.2f %5.2f %5.2f\n'%tuple(kl2[1])
+        strings=strings+'end Kpoint_Path\n\n'
+        return strings
+    def get_projection():
+        strings='begin projections\n'
+        for prj in projection:
+            strings=strings+'%s:%s\n'%prj
+        strings=strings+'end projections\n\n'
+        return strings
+    def get_grid(k_num):
+        if isinstance(k_num,int):
+            klist=tuple([k_num]*3)
+        elif isinstance(k_num,int):
+            if len(k_num)==2:
+                klist=(k_num[0],k_num[0],k_num[1])
+            else:
+                klist=tuple(k_num)
+        strings='mp_grid = %5d %5d %5d\n\n'%klist
+        return strings
+    ef=get_ef(prefix,'nscf')
+
+    num_val=[['num_bands',nband],['num_wann',nwann]]
+    num_strings=win_strings(num_val,'num')
+
+    dis_val=[['dis_win_max',dis_win[0]+ef],['dis_win_min',dis_win[1]+ef],
+             ['dis_froz_max',frz_win[0]+ef],['dis_froz_min',frz_win[1]+ef],['dis_num_iter',300]]
+    dis_strings=win_strings(dis_val,'dis')
+
+    plot_val=[['fermi_surface_plot',TorF(sw_fs_plot)],['fermi_energy',ef],
+              ['fermi_surface_num_points',fermi_mesh],['bands_plot','.True.']]
+    plot_strings=win_strings(plot_val,'plot')
+
+    k_path_strings=get_k_point_path()
+    unit_cell_strings='begin unit_cell_cart\n'+cell_parameter_stream(axis,deg)+'end unit_cell_cart\n\n'
+    atom_strings='begin atoms_frac\n'+atom_position(atom,atomic_position)+'end atoms_frac\n\n'
+    prj_strings=get_projection()
+    k_grid_strings=get_grid(k_mesh_wannier)
+    k_point_strings='begin kpoints\n'+k_cube_stream(k_mesh_wannier,F,T)+'end kpoints\n'
+
+    fname='%s.win'%prefix
+    fstream=(num_strings+dis_strings+plot_strings+k_path_strings+unit_cell_strings
+             +atom_strings+prj_strings+k_grid_strings+k_point_strings)
+    write_file(fname,fstream)
 
 def make_ph_in():
     fname='%s.ph'%prefix
     fstream='%s\n'%prefix
     var_inputph=['tr2_ph','prefix','fildyn','trans','ldisp','recover',
                  'outdir','fildvscf','electron_phonon','nq1','nq2','nq3']
-    val_inputph={'tr2_ph':1.0e-14,'prefix':"'%s'"%prefix,'fildyn':fildyn,'fildvscf':fildvscf,
+    val_inputph={'tr2_ph':w_conv(ph_conv),'prefix':"'%s'"%prefix,'fildyn':fildyn,'fildvscf':fildvscf,
                  'outdir':"'%s'"%outdir,'trans':'.True.','ldisp':'.True.','recover':recover,
                  'electron_phonon':"'interpolated'",
                  'nq1':q_mesh_dyn[0],'nq2':q_mesh_dyn[1],'nq3':q_mesh_dyn[2]}
@@ -424,7 +499,7 @@ def make_plotband_in(mode,win):
         fname='eband.plotband'
         name=prefix
         fext='band'
-        ef=get_ef()
+        ef=get_ef(prefix,'scf')
         win_min=win[0]+ef
         win_max=win[1]+ef
     else:
@@ -454,6 +529,16 @@ def main(prefix):
             os_and_print('plotbands.x <eband.plotband>eband.plotband.out')
         if sw_wan:
             make_pw_in('nscf')
+            if sw_run:
+                os_and_print('%s pw.x -nk %d <%s.nscf>%s.nscf.out'%(mpiexe,kthreads_num,prefix,prefix))
+            make_win()
+            if sw_run:
+                os_and_print('wannier90.x -pp %s'%(mpiexe,prefix))
+            make_pw2wan_in()
+            if sw_run:
+                os_and_print('%s pw2wan.x -nk %d <%s.pw2wan>%s.pw2wan.out'%(mpiexe,kthreads_num,prefix,prefix))
+            if sw_run:
+                os_and_print('wannier90.x %s'%(mpiexe,prefix))
     if sw_ph:
         make_ph_in()
         if sw_run:
